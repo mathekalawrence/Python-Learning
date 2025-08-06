8a59bdcb2cc55d839780f8be74f452719d8c3ba1
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-06 11:59:49 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,9 @@
+#Python variables
+#assigning values to a variable
+
+the_site = "My learning at PowerLearn"
+here_home = "Nothing better than doing the best"
+math = 788*45/24
+print(the_site)
+print(here_home)
+print("The result is: ", math)
