2b8cfafaa4dd94b0c477a5208e5bda5a6a1bfd28
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-06 12:04:31 +0300

    Add files via upload

--- lesson1.py
+++ lesson1.py
@@ -4,6 +4,10 @@
 the_site = "My learning at PowerLearn"
 here_home = "Nothing better than doing the best"
 math = 788*45/24
+h1 = 568.988
+h2 = 98720
+h3_boolean = True | False
 print(the_site)
 print(here_home)
-print("The result is: ", math)
+print("The result is: ", math, h1, h2)
+print(h3_boolean)
