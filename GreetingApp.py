d2abff509b4579cdf802ced448ef12a82b962e7a
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-07 13:00:49 +0300

    Add files via upload

---
+++ GreetingApp.py
@@ -0,0 +1,10 @@
+#A greeting app
+
+#def main():
+name = input("What's your name? ")
+color = input("What's your favorite color? ")
+
+print(f"\n Hello.. {name}! Your favorite color, {color}, is awesome!")
+
+#if name == "main":
+  #  main()
